33db40bf09556514bc53d32da2b2547da9577671
dpeps456789 <dpeps456789@naver.com>
2023-04-26 17:49:53 +0900

    2023.04.26

--- src/main/java/com/toyproject/bookmanagement/dto/book/GetBookRespDto.java
+++ src/main/java/com/toyproject/bookmanagement/dto/book/GetBookRespDto.java
@@ -3,17 +3,13 @@ package com.toyproject.bookmanagement.dto.book;
 import lombok.Builder;
 import lombok.Data;
 
-@Builder
 @Data
-public class SearchBookRespDto {
+@Builder
+public class GetBookRespDto {		// 책을 눌렀을때 화면
 	private int bookId;
 	private String bookName;
-	private int authorId;
 	private String authorName;
-	private int publisherId;
 	private String publisherName;
-	private int categoryId;
 	private String categoryName;
 	private String coverImgUrl;
-	private int likeCount;
 }
