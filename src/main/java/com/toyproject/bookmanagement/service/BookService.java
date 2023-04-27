58a21c2efb9687d9d446fe30483856408c6ccaf8
dpeps456789 <dpeps456789@naver.com>
2023-04-27 14:49:02 +0900

    백업용

--- src/main/java/com/toyproject/bookmanagement/service/BookService.java
+++ src/main/java/com/toyproject/bookmanagement/service/BookService.java
@@ -5,11 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.toyproject.bookmanagement.dto.book.CategoryRespDto;
 import com.toyproject.bookmanagement.dto.book.GetBookRespDto;
+import com.toyproject.bookmanagement.dto.book.RentalListRespDto;
 import com.toyproject.bookmanagement.dto.book.SearchBookReqDto;
 import com.toyproject.bookmanagement.dto.book.SearchBookRespDto;
 import com.toyproject.bookmanagement.entity.User;
@@ -64,14 +64,37 @@ public class BookService {
 		return bookRepository.getLikeCount(bookId);
 	}
 	
-	public int getLikeStatus(int bookId) {
+	public int getLikeStatus(int bookId, int userId) {
 		Map<String, Object> map = new HashMap<>();
 		map.put("bookId", bookId);
-		String email = SecurityContextHolder.getContext().getAuthentication().getName();
-		User userEntity = userRepository.findUserByEmail(email);
-		map.put("userId", userEntity.getUserId());
+		map.put("userId", userId);
 		
 		return bookRepository.getLikeStatus(map);
+	}
+	
+	public int setLike(int bookId, int userId) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("bookId", bookId);
+		map.put("userId", userId);
+		
+		return bookRepository.setLike(map);
+	}
+	
+	public int disLike(int bookId, int userId) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("bookId", bookId);
+		map.put("userId", userId);
+		
+		return bookRepository.disLike(map);
+	}
+	
+	public List<RentalListRespDto> getRentalListByBookId(int bookId) {
+		List<RentalListRespDto> list = new ArrayList<>();
+		bookRepository.getRentalListByBookId(bookId).forEach(rentalData -> {
+			list.add(rentalData.toDto());
+		});
+		
+		return list;
 	}
 }
 
